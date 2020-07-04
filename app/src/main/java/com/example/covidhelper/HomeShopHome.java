0821a8f8fc81648f09a5abe24dd8dc5dd006d245
VladUdri <vladudri@gmail.com>
2020-07-04 19:29:48 +0300

    navbar

---
+++ app/src/main/java/com/example/covidhelper/HomeShopHome.java
@@ -0,0 +1,48 @@
+package com.example.covidhelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class HomeShopHome extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_sh);
+
+        Toolbar toolbar = findViewById(R.id.idToolBarSH);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.idDrawerSH);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_close, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed()  {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+}
--- app/src/main/java/com/example/covidhelper/ShopHome.java
+++ app/src/main/java/com/example/covidhelper/ShopHome.java
@@ -1,8 +1,12 @@
 package com.example.covidhelper;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ShopHome extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class ShopHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_home);
+    }
+
+    @Nullable
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_sh, container, false);
     }
 }
